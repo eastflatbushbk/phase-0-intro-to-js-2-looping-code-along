4cf1ddc80f190a92c8c15460b61bd7a4b1adc030
Robert Sainton <robertsainton@outlook.com>
2022-03-03 16:45:10 -0500

    commit tree ec140561021746461b5092e5e00cec09d1610b84

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+function writeCards(array,string) {
+  
+    for (let i = 0; i < array.length; i++) {
+      var copyOfArray1 = "Thank you, "+ array[0] + ", for the wonderful " + string + " gift!";
+      var copyOfArray2 = "Thank you, "+ array[1] + ", for the wonderful " + string + " gift!";
+      var copyOfArray3 = "Thank you, "+ array[2] + ", for the wonderful " + string + " gift!";
+            
+    }
+  return [copyOfArray1, copyOfArray2, copyOfArray3] ;
+}
+ 
+function countDown(){
+        let count = 10 ;
+      
+      while (count > 0 ) {
+      console.log(count--);
+      
+      
+}
+console.log(0);
+return count;
+}
+    
+      
+
+
